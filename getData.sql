0b95708dff88c5fff58d6313df121612dfbf6383
nashdev255 <yutori1107@gmail.com>
2023-09-21 20:50:17 +0900

    where

--- getData.sql
+++ getData.sql
@@ -2,3 +2,10 @@
 SELECT name FROM user;
 SELECT id, name FROM user;
 SELECT * FROM user;
+
+/* 特定の要素を持つuserのレコードを取得する */
+SELECT * FROM user WHERE name = "Bob";
+SELECT * FROM user WHERE registered_at = "2023-09-21";
+SELECT * FROM user WHERE id = 100;
+SELECT * FROM user WHERE number_of_followers >= 1000;
+SELECT * FROM user WHERE registered_at >= "2022-01-01";
