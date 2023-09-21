214f7992e8c421c74fd8635dfe666935b8aa6f08
nashdev255 <yutori1107@gmail.com>
2023-09-22 01:05:12 +0900

    sum, avg

--- editData.sql
+++ editData.sql
@@ -3,3 +3,10 @@ SELECT DISTINCT(name) FROM items;
 
 /* 四則演算した状態で取得する */
 SELECT price * 1.08 FROM items;
+SELECT price - 30 FROM items;
+
+/* 特定のカラムの合計値を取得する */
+SELECT SUM(price) FROM items WHERE category = "Beauty";
+
+/* 特定のカラムの平均値を取得する */
+SELECT SUM(number_of_followers) FROM user WHERE name LIKE "%Official%";
